83a8ce81d3262976aa11e12fe84a1f2449d9d649
Rebecca Poulson <rpoulson@stanford.edu>
2012-04-20 12:42:37 -0700

    Test: 4.20.12

---
+++ rebeccapoulson/Desktop/Lembic_Full/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LembicFull::Application.config.secret_token = '389e512f1f5122626194e5333c45c3eb41100da112094519ff08d28d0c53534109b6427fac12c3293a3e070c5ccb60cd8bd1cf6cf46b6ec71ff19bc79845baa6'
---
+++ rebeccapoulson/Desktop/myapp/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myapp::Application.config.secret_token = 'ec8c80c1274a781c5e43f8a57f8ade8128e7243c59817a750ebb3590d686ffba0ca775742113c12288318d80e42b234f8974ab76d496ca49acd2a35c09eef6b0'

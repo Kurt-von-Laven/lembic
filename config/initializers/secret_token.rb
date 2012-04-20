f49082316aae3b278a76d0f8d565cd3d08a4e3fe
Carbon Lighthouse Server <kurt.von.laven@gmail.com>
2012-04-19 19:42:16 -0600

    Created default Rails 3.2.3 app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lembic::Application.config.secret_token = '300468eed491e5c8830fb61d0ef961b8d778f6e353aa44597649ba2c3297a9ff212fc667c707c135f1d542e9c52e230d40d2d3f0773fcc46f401614307d82d09'

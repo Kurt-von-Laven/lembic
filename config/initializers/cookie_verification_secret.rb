bf955330041ea0a93f4d8eba53bc8bf7ed6f79ea
Carbon Lighthouse Server <kurt.von.laven@gmail.com>
2012-04-19 19:35:40 -0600

    Deleted everything.

--- config/initializers/cookie_verification_secret.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.cookie_verifier_secret = 'fe2ca03eac71e63e4863a6d132401a66efea99cea17e68a2e829b6f5b161e071dbc126d9349f4c31367c89392b0314dae204947018056b256d09e9e81dd791ef';

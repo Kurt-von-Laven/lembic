ea2ac6ea3a72ed9e2b6ef21c8c38ab2420c9655e
Kurt von Laven <kurt.von.laven@gmail.com>
2012-04-30 21:20:29 -0700

    Cleaned up whitespace in editor controller.

--- app/controllers/editor_controller.rb
+++ app/controllers/editor_controller.rb
@@ -1,11 +1,12 @@
 class EditorController < ApplicationController
   def home
+    # No changes
   end
-
-	def variableeditor
-        @new_var = Variable.new(params[:var])
-        @new_var.save
-        @variables = Variable.find(:all)
-        render 'variableeditor'
-	end
+  
+  def variableeditor
+    @new_var = Variable.new(params[:var])
+    @new_var.save
+    @variables = Variable.find(:all)
+    render 'variableeditor'
+  end
 end

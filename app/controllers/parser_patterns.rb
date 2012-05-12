11b715e8feb9cff81ce1ad623e6b4b7669d644e2
Ben Christel <ben.christel@gmail.com>
2012-05-12 14:32:12 -0700

    started csv to lembic conversion module

--- app/controllers/parser_patterns.rb
+++ app/controllers/parser_patterns.rb
@@ -1,3 +1,22 @@
+# Parser Pattern Syntax
+#
+# A parser pattern is a sequence of templates that the parser is trying to match to the input sequence.
+# For example, a parser pattern could be designed to match the following sequence:
+# "(", followed by one or zero tokens of type <expression>, followed by zero or more sequences of the form "," <expression>, followed by ")".
+#
+# Semi-formal syntax:
+# parser_pattern -> [<munch_template>*]
+# munch_template -> {:min_munches => <int>, :max_munches => <int>, :munch_pattern => <munch_pattern> }
+# munch_pattern -> [<token_template>*]
+# token_template -> {:criterion => <criterion>, :match => <object>, :capturing => <boolean> }
+# criterion -> :type | :value | :regex
+#
+# A munch is a sub-pattern that matches several tokens at once.  A munch_template
+# specifies a pattern for the munch, as well as the minimum and maximum number
+# of times the munch can be repeated.  For example, a munch could be constructed that matches
+# the pattern "a", "b" if the pattern repeats at most three times.
+
+
 class ParserPatterns
   
   def self.infix_operator_pattern(op_regex)

e8d5e0242340dca5e5c9f3cda3d895b8d6395226
Kurt von Laven <kurt.von.laven@gmail.com>
2012-05-15 16:47:52 -0700

    Successfully read a file server-side but didn't do anything with its contents.

--- app/models/variable.rb
+++ app/models/variable.rb
@@ -1,7 +1,7 @@
 class Variable < ActiveRecord::Base
   attr_accessible :name, :description, :workflow_id, :variable_type, :array, :created_at, :updated_at, :expression_string, :expression_object
   
-  DEFAULT_OPTIONS = {'workflow_id' => 1, 'array' => 0} # TODO: Grab the workflow ID out of the session state.
+  DEFAULT_OPTIONS = {'array' => 0}
   
   # type is an integer in range X to X, inclusive.  The mapping is as follows:
   #
@@ -33,6 +33,7 @@ class Variable < ActiveRecord::Base
     merged_var = DEFAULT_OPTIONS.merge(form_hash)
     merged_var['created_at'] = now
     merged_var['updated_at'] = now
+    merged_var['workflow_id'] = 1 # TODO: Grab the workflow ID out of the session state.
     merged_var['variable_type'] = merged_var['variable_type'].to_i
     merged_var['array'] = merged_var['array'].to_i
     if merged_var['expression_string'].empty?
@@ -42,6 +43,26 @@ class Variable < ActiveRecord::Base
       merged_var['expression_object'] = parser.parse(merged_var['expression_string'])
     end
     Variable.create(merged_var)
+  end
+  
+  def self.create_constant_array(form_hash)
+    now = Time.now
+    Permission.where(:user_id => 1).first_or_create({'workflow_id' => 1, 'permissions' => 4, 'created_at' => now, 'updated_at' => now})
+    User.where(:first_name => 'Michael').first_or_create({'last_name' => 'Jones', 'email' => 'qweoui@adsfqw.com', 'organization' => 'City Team',
+                                                               'pwd_hash' => '21ad42ef24123589abcd', 'created_at' => now, 'updated_at' => now})
+    Workflow.where(:name => 'Sample Workflow').first_or_create({'description' => 'This record should be removed eventually and is just for test purposes.',
+                                                                     'created_at' => now, 'updated_at' => now})
+    merged_array = DEFAULT_OPTIONS.merge(form_hash)
+    merged_array['created_at'] = now
+    merged_array['updated_at'] = now
+    merged_array['workflow_id'] = 1 # TODO: Grab the workflow ID out of the session state.
+    merged_array['variable_type'] = merged_array['variable_type'].to_i
+    merged_array['array'] = 1
+    data_file = merged_array['data_file']
+    puts data_file.read
+    merged_array.delete('data_file')
+    merged_array.delete('column_number')
+    merged_array.delete('row_number')
   end
   
 end

193f04782946ea5361752dd3d0d36d4488ff84ae
Rebecca Poulson <rpoulson@stanford.edu>
2012-05-21 16:39:58 -0700

    slight edits to view editor, plus improved table code

--- app/controllers/workflow_controller.rb
+++ app/controllers/workflow_controller.rb
@@ -3,9 +3,10 @@ require './app/helpers/expression'
 class WorkflowController < ApplicationController
   
   def evaluate
-    variables = params[:variables]
-    if !variables.nil?
-      input_values = eval(variables['input_values'])
+       @variables = Variable.where(:workflow_id => session[:user_id]).order(:name)
+    vars = params[:variables]
+    if !vars.nil?
+      input_values = eval(vars['input_values'])
       for input_variable, input_formula in input_values
         input_values[input_variable] = {:value => input_formula}
       end
@@ -13,7 +14,7 @@ class WorkflowController < ApplicationController
         expression_object = variable.expression_object
         input_values[variable.name] = {:formula => expression_object} unless expression_object.nil?
       end
-      output_variable = variables['output_variable']
+      output_variable = vars['output_variable']
       evaluator = Evaluator.new
       evaluator.eval_variable(output_variable, input_values, nil)
       @results = input_values

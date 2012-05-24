c468e6434c633e0cde5dfea05bb4e207a054d700
Thomas Boyce <tboyce@stanford.edu>
2012-05-23 18:37:09 -0700

    Made it so user can enter block inputs and block connections independently from blocks themselves

--- app/controllers/view_editor_controller.rb
+++ app/controllers/view_editor_controller.rb
@@ -5,9 +5,20 @@ class ViewEditorController < ApplicationController
       form_hash = params[:create_block_form]
       if !form_hash.nil?
         
-        # Create a block with the specified name
-        block_hash = {:name => form_hash[:name], :workflow_id => session[:user_id]}
-        block = Block.create(block_hash)
+        # Create a block with the specified name and workflow_id
+        name = form_hash[:name]
+        workflow_id = session[:user_id]
+        block = Block.create({:name => name, :workflow_id => workflow_id})
+      end
+      
+      # Check for form data for creating a block_input
+      form_hash = params[:create_block_inputs_form]
+      if !form_hash.nil?
+        
+        # Find the block these inputs are for
+        block_name = form_hash[:block_name]
+        block = Block.find_by_name(block_name)
+        # TODO: check for error finding block
         
         # Iterate through lines in the inputs string
         form_hash[:inputs_string].lines do |line|
@@ -18,10 +29,24 @@ class ViewEditorController < ApplicationController
             next
           end
           
+          # Determine sort_index
+          sort_index = 0
+          
           # Create a block input with the specified variable
-          block.block_inputs.create({:variable => variable_name, :sort_index => 0})
+          block.block_inputs.create({:variable => variable_name, :sort_index => sort_index})
         end
+      end
+      
+      # Check for form data for creating a block_connection
+      form_hash = params[:create_block_connections_form]
+      if !form_hash.nil?
         
+        # Find the block these inputs are for
+        block_name = form_hash[:block_name]
+        block = Block.find_by_name(block_name)
+        # TODO: check for error finding block
+        
+        # Create parser for parsing expression strings
         parser = Parser.new
         
         # Iterate through lines in the connections string
@@ -35,17 +60,21 @@ class ViewEditorController < ApplicationController
           next_block = tokens[0].strip
           expression_string = tokens[1].strip
           
+          # Parse the expression into an object, using parser
           expression_object = parser.parse(expression_string) # TODO: Catch and handle parser errors.
           
+          # Determine the sort_index
+          sort_index = 0
+          
           # Create a block connection with the specified next_block and expression
-          block_connection_hash = {:next_block => next_block, :expression_string => expression_string, :expression_object => expression_object}
+          block_connection_hash = {:next_block => next_block, :expression_string => expression_string, :expression_object => expression_object, :sort_index => sort_index}
           block.block_connections.create(block_connection_hash)
         end
       end
       
       # Set variables used by views for rendering
       @variables = Variable.where(:workflow_id => session[:user_id]).order(:name)
-      @blocks = Block.find(:all)
+      @blocks = Block.where(:workflow_id => session[:user_id]).order(:name)
       
   end
 

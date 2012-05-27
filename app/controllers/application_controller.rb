d39e363482027c9d203d610b72b3d0d92552338d
Kurt von Laven <kurt.von.laven@gmail.com>
2012-05-27 16:52:50 -0700

    Fixed the logout loophole by turning off caching.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   
-  before_filter :verify_login
+  before_filter :prevent_caching, :verify_login
+  
+  def prevent_caching
+    response.headers['Cache-Control'] = 'no-cache, no-store, max-age=0, must-revalidate'
+    response.headers['Pragma'] = 'no-cache'
+    response.headers['Expires'] = 'Thu, 01 Jan 1970 00:00:00 GMT'
+  end
   
   def verify_login
     if User.where(:id => session[:user_id]).empty?

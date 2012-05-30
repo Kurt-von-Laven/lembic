c46b5639edb7e144f25708a219de89ee484dbc25
Ben Christel <ben.christel@gmail.com>
2012-05-30 15:42:04 -0700

    wrote migration to delete variables with duplicate names

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120530220716) do
+ActiveRecord::Schema.define(:version => 20120530222904) do
 
   create_table "block_connections", :force => true do |t|
     t.string   "expression_string", :limit => 1048576, :null => false

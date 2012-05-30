e1ecbde5e0b51c9abab677dbbdf7565fc67fa261
Kurt von Laven <kurt.von.laven@gmail.com>
2012-05-30 12:27:13 -0700

    Halfway through patching the evaluator security bug.

--- app/controllers/workflow_controller.rb
+++ app/controllers/workflow_controller.rb
@@ -1,15 +1,18 @@
-require './app/helpers/expression'
+require Rails.root.join('app/helpers/expression')
 
 class WorkflowController < ApplicationController
   
   def evaluate
     @variables = Variable.where(:workflow_id => session[:user_id]).order(:name)
+    @input_variables = Variable.where(:workflow_id => session[:user_id], :expression_string => nil).order(:name)
     vars = params[:evaluator]
     if !vars.nil?
-      input_values = eval(vars['input_values'])
-      output_variables = eval(vars['output_variables'])
-      for input_variable, input_formula in input_values
-        input_values[input_variable] = {:value => input_formula}
+      input_values = vars['input_values']
+      input_values_hash = {}
+      i = 0
+      for input_value in input_values
+        input_values_hash[@input_variables[i].name] = {:value => input_value}
+        i += 1
       end
       Variable.find(:all).each do |variable|
         expression_object = variable.expression_object
@@ -22,15 +25,16 @@ class WorkflowController < ApplicationController
           index_names = nil
           varname = variable.name
         end
-        input_values[varname] ||= {}
-        input_values[varname][:formula] = expression_object unless expression_object.nil?
-        input_values[varname][:index_names] = index_names if index_names
+        input_values_hash[varname] ||= {}
+        input_values_hash[varname][:formula] = expression_object unless expression_object.nil?
+        input_values_hash[varname][:index_names] = index_names if index_names
       end
       evaluator = Evaluator.new
-      evaluator.eval_all(output_variables, input_values)
-      @results = input_values
+      evaluator.eval_all([], input_values_hash) # array of hashes mapping :name => variable_name, {:begin}
+      @output_variables = input_values_hash
+      logger.debug(input_values_hash.inspect)
     else
-      @results = []
+      @output_variables = []
     end
     render 'evaluator'
   end

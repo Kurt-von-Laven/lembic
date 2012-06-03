1203ec60080de3174f9c1cd5c489cca8f6ed8078
Kurt von Laven <kurt.von.laven@gmail.com>
2012-06-02 23:44:24 -0700

    Fixed a migration that crashed in production.

---
+++ db/migrate/20120530235252_delete_all_blocks.rb
@@ -0,0 +1,10 @@
+class DestroyAllBlocks < ActiveRecord::Migration
+  def up
+    Block.delete_all
+    BlockInput.delete_all
+    BlockConnection.delete_all
+  end
+
+  def down
+  end
+end
--- db/migrate/20120530235252_destroy_all_blocks.rb
+++
@@ -1,9 +0,0 @@
-class DestroyAllBlocks < ActiveRecord::Migration
-  def up
-    # Should also destroy all block_inputs and block_connections due to :dependent => :destroy
-    Block.destroy_all
-  end
-
-  def down
-  end
-end

3eac00aa262f7b313999e9e8aa0fc8233c349f75
Ben Christel <ben.christel@gmail.com>
2012-06-03 20:02:53 -0700

    fixed run_values migration

--- db/migrate/20120604024915_create_run_values.rb
+++ db/migrate/20120604024915_create_run_values.rb
@@ -1,7 +1,6 @@
 class CreateRunValues < ActiveRecord::Migration
   def change
     create_table :run_values do |t|
-      t.primary_key :id, :null => false
       t.integer :variable_id, :null => false
       t.integer :run_id, :null => false
       t.string :index_values

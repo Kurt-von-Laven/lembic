0b2976af038d06849a9b669d13e968aeabc5b853
Kurt von Laven <kurt.von.laven@gmail.com>
2012-06-03 22:51:37 -0700

    Turned off validation in a migration that was crashing for Kseniya with validation.

--- db/migrate/20120530220716_strip_indices_from_variable_names.rb
+++ db/migrate/20120530220716_strip_indices_from_variable_names.rb
@@ -5,7 +5,7 @@ class StripIndicesFromVariableNames < ActiveRecord::Migration
     all_variables = Variable.find(:all)
     all_variables.each do |v|
       v.name = v.name.split(/\s*\[\s*/)[0]
-      v.save
+      v.save(:validate => false)
     end
   end
 end

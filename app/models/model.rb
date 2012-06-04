2af636d78660e760af7b89bddfded45f27c7c2ee
Kurt von Laven <kurt.von.laven@gmail.com>
2012-06-03 20:57:02 -0700

    Corrected a has_many relationship for the model model.

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,14 +1,14 @@
 class Model < ActiveRecord::Base
   
   # Associations
-  has_many :permissions, :dependent => :destroy
+  has_many :model_permissions, :dependent => :destroy
   has_many :variables, :dependent => :destroy
   has_many :workflows, :dependent => :destroy
   
   # Validators
   validates_presence_of :name, :description
   validates_uniqueness_of :name
-  validates_associated :permissions, :variables, :workflows
+  validates_associated :model_permissions, :variables, :workflows
   
   # Attribute accessors
   attr_accessible :id, :description, :name

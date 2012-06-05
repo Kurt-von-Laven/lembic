499fed00f592de1385ac849ed440cc702f54692b
Kurt von Laven <kurt.von.laven@gmail.com>
2012-06-04 21:19:12 -0700

    Removed a no longer appropriate validation.

--- app/helpers/condense_sort_indices.rb
+++ app/helpers/condense_sort_indices.rb
@@ -1,14 +1,10 @@
 module CondenseSortIndices
   
   def self.included(base)
-    
-  end
-  
-  def condense_sort_indices(scope)
     # Decrement higher sort_indices to prevent sparseness
-    self.after_destroy do |destroyed|
-      self.transaction do
-        self.where("#{scope.to_s} = ? AND sort_index > ?", destroyed.send(scope), destroyed.sort_index).order(:sort_index).each do |record|
+    base.after_destroy do |destroyed|
+      base.transaction do
+        base.where("#{INDEX_SCOPE.to_s} = ? AND sort_index > ?", destroyed.send(INDEX_SCOPE), destroyed.sort_index).order(:sort_index).each do |record|
           record.sort_index -= 1
           record.save
         end

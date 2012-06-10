f9e4b97c5183544ca15499f39fa96556eeff2089
Kurt von Laven <kurt.von.laven@gmail.com>
2012-06-10 10:39:26 -0700

    Took out a destroy_all that was causing a migration to crash.

--- db/migrate/20120610163552_add_sort_index_to_workflow.rb
+++ db/migrate/20120610163552_add_sort_index_to_workflow.rb
@@ -1,6 +1,7 @@
 class AddSortIndexToWorkflow < ActiveRecord::Migration
   def change
-    Workflow.destroy_all
+    Workflow.delete_all
+    Block.destroy_all
     add_column :workflows, :sort_index, :integer, :null => false
   end
 end
